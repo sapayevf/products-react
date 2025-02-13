090bb6f558018a561874f67282e08dcce3902136
sapayevf <f.sapayevv@gmail.com>
2025-02-14 02:22:25 +0500

    project end

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -2,19 +2,36 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import Card from "../Card/Card";
 import "./Products.scss";
+import Skeleton from "@mui/material/Skeleton";
+import { Grid, Box } from "@mui/material";
 
 function Products() {
   const [products, setProducts] = useState([]);
   const [search, setSearch] = useState("");
   const [sortPrice, setSortPrice] = useState("");
   const [sortRating, setSortRating] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [count, setCount] = useState(1);
+  const [dataLength, setDataLength] = useState(0);
+
+  const limit = 30;
 
   useEffect(() => {
+    setLoading(true);
     axios
-      .get("https://dummyjson.com/products")
-      .then((res) => setProducts(res.data.products))
-      .catch((error) => console.error("Error fetching products:", error));
-  }, []);
+      .get("https://dummyjson.com/products", {
+        params: {
+          limit: limit * count,
+        },
+      })
+      .then((res) => {
+        setProducts(res.data.products);
+        setDataLength(res.data.total);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }, [count]);
 
   const filteredProducts = products
     .filter((product) =>
@@ -52,11 +69,36 @@ function Products() {
         </select>
       </div>
 
-      <div className="product-cards">
-        {filteredProducts.map((product) => (
-          <Card key={product.id} product={product} />
-        ))}
-      </div>
+      {loading ? (
+        <Grid container spacing={2} sx={{ width: "100%", mt: 2 }}>
+          {Array.from({ length: 9 }).map((_, index) => (
+            <Grid item key={index} xs={12} sm={6} md={4}>
+              <Box>
+                <Skeleton variant="rectangular" width="100%" height={150} />
+                <Skeleton />
+                <Skeleton width="60%" />
+              </Box>
+            </Grid>
+          ))}
+        </Grid>
+      ) : (
+        <div className="product-cards">
+          {filteredProducts.map((product) => (
+            <Card key={product.id} product={product} />
+          ))}
+        </div>
+      )}
+
+      {products.length > 0 && dataLength > limit * count ? (
+        <button
+          onClick={() => setCount((p) => p + 1)}
+          className="show-more-btn"
+        >
+          Show More
+        </button>
+      ) : (
+        ""
+      )}
     </div>
   );
 }
